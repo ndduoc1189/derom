c99f3128cd4866cf71c00dc321509891b7b4562d
ndduoc1189 <110657370+ndduoc1189@users.noreply.github.com>
2023-03-17 08:42:10 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "DERO START MINING ----------------------------------"
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qy2putx0dw6ln7229l4pmragg8tdc4lw557c02zd03283ndtm00zxqg0qezdh -r dero.friendspool.club:10300 -r1 community-pools.mysrv.cloud:10300 -r2 dero.rabidmining.com:10300 -m 7 -p rpc;
+    ./astrominer -w dero1qy2putx0dw6ln7229l4pmragg8tdc4lw557c02zd03283ndtm00zxqg0qezdh -r dero.friendspool.club:10300 -r1 community-pools.mysrv.cloud:10300 -r2 dero.rabidmining.com:10300 -m 8 -p rpc;
     sleep 5;
 done
